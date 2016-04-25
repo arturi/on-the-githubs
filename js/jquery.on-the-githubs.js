8af3c28b8fb2f47e18a418638e92aabdf460b7c7
Artur Paikin <artur@arturpaikin.com>
2016-04-25 12:00:14 -0400

    Add html-escaping for comments, replace ugly quotes with pretty ones
    [This comment](https://github.com/transloadit/uppy/commit/7ffc0ee9907dbadd3b8cdebdd0d6296d8b95dea2#commitcomment-17202727) broke the layout.

--- js/jquery.on-the-githubs.js
+++ js/jquery.on-the-githubs.js
@@ -1,224 +1,235 @@
 (function( $ ) {
 
-  $.fn.onthegithubs = function( options ) {
+	$.fn.onthegithubs = function( options ) {
 
     var settings = $.extend({
-      // event-source: "repos/kvz/nsfailover",
-    }, options || {});
-    var self = this;
+			// event-source: "repos/kvz/nsfailover",
+		}, options || {});
+		var self = this;
 
-    var makeHtmlUrl = function (url) {
-      var result = url;
+		var makeHtmlUrl = function (url) {
+			var result = url;
 
-      result = result.replace(/\/users\//, '/');
-      result = result.replace(/\/repos\//, '/');
-      result = result.replace(/\/api\./, '/');
-      result = result.replace(/\/commits\//, '/commit/');
+			result = result.replace(/\/users\//, '/');
+			result = result.replace(/\/repos\//, '/');
+			result = result.replace(/\/api\./, '/');
+			result = result.replace(/\/commits\//, '/commit/');
 
-      return result;
-    };
+			return result;
+		};
 
-    var templater = function (template, vars) {
-      return template.replace(/\{([^\}]*)\}/g, function(m, key) {
-        return vars.hasOwnProperty(key) ? vars[key] : "";
-      });
-    };
+		var templater = function (template, vars) {
+			return template.replace(/\{([^\}]*)\}/g, function(m, key) {
+				return vars.hasOwnProperty(key) ? vars[key] : "";
+			});
+		};
 
-    var download = function (source) {
-      if (!source) {
-        throw Error('Please set an event source like "orgs/tus" via data-event-source attr or options');
-      }
+		var escapeHtml = function (unsafe) {
+			return unsafe
+					 .replace(/&/g, "&amp;")
+					 .replace(/</g, "&lt;")
+					 .replace(/>/g, "&gt;")
+					 .replace(/"/g, "&quot;")
+					 .replace(/'/g, "&#039;");
+		};
 
-      var d       = $.Deferred();
-      var githubs = localStorage.getItem('on-the-githubs-feed-' + source);
-      var time    = localStorage.getItem('on-the-githubs-date-' + source);
-      var now     = +new Date() / 1000;
-      var url     = 'https://api.github.com/' + source + '/events?per_page=20&callback=?';
+		var download = function (source) {
+			if (!source) {
+				throw Error('Please set an event source like "orgs/tus" via data-event-source attr or options');
+			}
 
-      githubs = JSON.parse(githubs);
-      if (githubs && githubs.data && time && now - time < 3 * 60) {
-        d.resolve(githubs);
-        return d;
-      }
+			var d			 = $.Deferred();
+			var githubs = localStorage.getItem('on-the-githubs-feed-' + source);
+			var time		= localStorage.getItem('on-the-githubs-date-' + source);
+			var now		 = +new Date() / 1000;
+			var url		 = 'https://api.github.com/' + source + '/events?per_page=20&callback=?';
 
-      $.getJSON(url, function(data, textStatus, jqXHR) {
-        if (data.data.message) {
-          throw Error('GitHub says: ' + data.data.message + ' for url: ' + url);
-        }
-        data.data = data.data.slice(0, 20);
+			githubs = JSON.parse(githubs);
+			if (githubs && githubs.data && time && now - time < 3 * 60) {
+				d.resolve(githubs);
+				return d;
+			}
 
-        localStorage.setItem('on-the-githubs-feed-' + source, JSON.stringify(data));
-        localStorage.setItem('on-the-githubs-date-' + source, now);
-        d.resolve(data);
-      });
+			$.getJSON(url, function(data, textStatus, jqXHR) {
+				if (data.data.message) {
+					throw Error('GitHub says: ' + data.data.message + ' for url: ' + url);
+				}
+				data.data = data.data.slice(0, 20);
 
-      return d;
-    };
+				localStorage.setItem('on-the-githubs-feed-' + source, JSON.stringify(data));
+				localStorage.setItem('on-the-githubs-date-' + source, now);
+				d.resolve(data);
+			});
 
-    return this.each(function() {
-      var $obj  = $(this);
-      var eventSource;
-      var val;
-      if ((val = settings['event-source'])) {
-        eventSource = val;
-      } else if ((val = $obj.data('event-source'))) {
-        eventSource = val;
-      }
+			return d;
+		};
 
-      download(eventSource).done(function (data) {
-        $obj.html('');
+		return this.each(function() {
+			var $obj	= $(this);
+			var eventSource;
+			var val;
+			if ((val = settings['event-source'])) {
+				eventSource = val;
+			} else if ((val = $obj.data('event-source'))) {
+				eventSource = val;
+			}
 
-        var template = '<abbr class="timeago" title="{created}">{created}</abbr>';
-        template    += '<img src="{gravatarSrc}" class="gravatar" />';
-        template    += '<p><a target="_blank" href="{userUrl}" class="author">{username}</a> ';
-        template    += '<span>{action}</span>';
-        template    += ' <a target="_blank" href="{repoUrl}">{repoName}</a>{branch}{commits}</p>';
-        template    += '<div class="clearfix"></div>';
+			download(eventSource).done(function (data) {
+				$obj.html('');
 
-        for (var i = 0; i < data.data.length; i++) {
-          var item = data.data[i];
+				var template = '<abbr class="timeago" title="{created}">{created}</abbr>';
+				template		+= '<img src="{gravatarSrc}" class="gravatar" />';
+				template		+= '<p><a target="_blank" href="{userUrl}" class="author">{username}</a> ';
+				template		+= '<span>{action}</span>';
+				template		+= ' <a target="_blank" href="{repoUrl}">{repoName}</a>{branch}{commits}</p>';
+				template		+= '<div class="clearfix"></div>';
 
-          var gravatarSrc = item.actor.avatar_url;
-          var username    = item.actor.login;
-          var action      = '';
-          var branch      = '';
-          var commits     = '';
+				for (var i = 0; i < data.data.length; i++) {
+					var item = data.data[i];
 
-          // http://developer.github.com/v3/activity/events/types/
-          var isClosed = false;
+					var gravatarSrc = item.actor.avatar_url;
+					var username		= item.actor.login;
+					var action			= '';
+					var branch			= '';
+					var commits		 = '';
 
-          switch (item.type) {
-            case 'IssuesEvent':
-              // console.log(item);
-              isClosed = item.payload.issue.state === 'closed';
-              action   = item.payload.action + ' ';
-              action  += 'issue <a target="_blank" href="' + item.payload.issue.html_url + '">';
+					// http://developer.github.com/v3/activity/events/types/
+					var isClosed = false;
 
-              if (isClosed) {
-                action += '<s>';
-              }
-              action += item.payload.issue.title;
-              if (isClosed) {
-                action += '</s>';
-              }
+					switch (item.type) {
+						case 'IssuesEvent':
+							// console.log(item);
+							isClosed = item.payload.issue.state === 'closed';
+							action	 = item.payload.action + ' ';
+							action	+= 'issue <a target="_blank" href="' + item.payload.issue.html_url + '">';
 
-              action += '</a> on';
-              break;
-            case 'CommitCommentEvent':
-              action  = 'commented "' + item.payload.comment.body + '" ';
-              action += 'on a commit to <a target="_blank" href="' + item.payload.comment.html_url + '">';
-              action += item.payload.comment.path + '</a> in';
-              break;
-            case 'IssueCommentEvent':
-              isClosed = item.payload.issue.state === 'closed';
+							if (isClosed) {
+								action += '<s>';
+							}
+							action += item.payload.issue.title;
+							if (isClosed) {
+								action += '</s>';
+							}
 
-              action  = 'commented on <a target="_blank" href="' + item.payload.comment.html_url + '">';
-              if (isClosed) {
-                action += '<s>';
-              }
-              action += item.payload.issue.title;
-              if (isClosed) {
-                action += '</s>';
-              }
-              action += '</a> in';
-              break;
-            case 'PushEvent':
-              // var commitString = item.payload.commits.length === 1 ? 'commit' : 'commits';
+							action += '</a> on';
+							break;
+						case 'CommitCommentEvent':
+							action	= 'commented “' + escapeHtml(item.payload.comment.body) + '” ';
+							action += 'on a commit to <a target="_blank" href="' + item.payload.comment.html_url + '">';
+							action += item.payload.comment.path + '</a> in';
+							break;
+						case 'IssueCommentEvent':
+							isClosed = item.payload.issue.state === 'closed';
 
-              if (!item.payload || !item.payload.commits || item.payload.commits.length === 0) {
-                break;
-              }
-              var firstCommit = item.payload.commits[0].sha;
-              var firstUrl    = makeHtmlUrl(item.payload.commits[0].url);
-              var lastCommit  = '';
-              var messages    = [];
-              var msg         = '';
-              for (var j in item.payload.commits) {
-                lastCommit = item.payload.commits[j].sha;
-                msg = item.payload.commits[j].message.substr(0, 50);
-                if (msg.length > item.payload.commits[j].message.length) {
-                  msg += ' ...';
-                }
-                messages.push(msg);
-              }
-              var compareUrl  = firstUrl.replace('commit', 'compare').replace(firstCommit, firstCommit + '...' + lastCommit);
-              var txtMessages = messages.join(' &mdash; ');
+							action	= 'commented on <a target="_blank" href="' + item.payload.comment.html_url + '">';
+							if (isClosed) {
+								action += '<s>';
+							}
+							action += escapeHtml(item.payload.issue.title);
+							if (isClosed) {
+								action += '</s>';
+							}
+							action += '</a> in';
+							break;
+						case 'PushEvent':
+							// var commitString = item.payload.commits.length === 1 ? 'commit' : 'commits';
 
-              action  = 'pushed ';
-              if (item.payload.commits.length === 1) {
-                action += '<a target="_blank" rel="tooltip" data-placement="right" title="' + txtMessages + '" href="' + firstUrl + '">';
-                action += '1 commit';
-                action += '</a>';
-              } else {
-                action += '<a target="_blank" rel="tooltip" data-placement="right" title="' + txtMessages + '" href="' + compareUrl + '">';
-                action += item.payload.commits.length + ' ' + 'commits';
-                action += '</a>';
-              }
-              action += ' to';
+							if (!item.payload || !item.payload.commits || item.payload.commits.length === 0) {
+								break;
+							}
+							var firstCommit = item.payload.commits[0].sha;
+							var firstUrl		= makeHtmlUrl(item.payload.commits[0].url);
+							var lastCommit	= '';
+							var messages		= [];
+							var msg				 = '';
+							for (var j in item.payload.commits) {
+								lastCommit = item.payload.commits[j].sha;
+								msg = item.payload.commits[j].message.substr(0, 50);
+								if (msg.length > item.payload.commits[j].message.length) {
+									msg += ' ...';
+								}
+								msg = escapeHtml(msg)
+								messages.push(msg);
+							}
+							var compareUrl	= firstUrl.replace('commit', 'compare').replace(firstCommit, firstCommit + '...' + lastCommit);
+							var txtMessages = messages.join(' &mdash; ');
+							txtMessages = escapeHtml(txtMessages)
 
-              // commits = '<ul class="commits">';
-              // for (var j in item.payload.commits) {
-              //   var commit = item.payload.commits[j];
+							action	= 'pushed ';
+							if (item.payload.commits.length === 1) {
+								action += '<a target="_blank" rel="tooltip" data-placement="right" title="' + txtMessages + '" href="' + firstUrl + '">';
+								action += '1 commit';
+								action += '</a>';
+							} else {
+								action += '<a target="_blank" rel="tooltip" data-placement="right" title="' + txtMessages + '" href="' + compareUrl + '">';
+								action += item.payload.commits.length + ' ' + 'commits';
+								action += '</a>';
+							}
+							action += ' to';
 
-              //   var sha = commit.sha.substr(0, 7);
-              //   var msg = commit.message.substr(0, 50);
-              //   if (msg.length !== commit.message.length) {
-              //     msg += ' ...';
-              //   }
+							// commits = '<ul class="commits">';
+							// for (var j in item.payload.commits) {
+							//	 var commit = item.payload.commits[j];
 
-              //   commits += '<li>';
-              //   commits += '<a href="' + makeHtmlUrl(commit.url) + '">' + sha + '</a>';
-              //   commits += ' ' + msg;
-              //   commits += '</li>';
-              // }
+							//	 var sha = commit.sha.substr(0, 7);
+							//	 var msg = commit.message.substr(0, 50);
+							//	 if (msg.length !== commit.message.length) {
+							//		 msg += ' ...';
+							//	 }
 
-              // commits += '</ul>';
-              // console.log(item.type, item);
-              break;
-            case 'ForkEvent':
-              action = 'forked';
-              break;
-            case 'WatchEvent':
-              action = 'is now watching';
-              break;
-            case 'PullRequestEvent':
-              action  = item.payload.action + ' ';
-              action += 'pull request <a target="_blank" href="' + item.payload.pull_request.html_url + '">';
-              action += item.payload.pull_request.title;
-              action += '</a> for';
+							//	 commits += '<li>';
+							//	 commits += '<a href="' + makeHtmlUrl(commit.url) + '">' + sha + '</a>';
+							//	 commits += ' ' + msg;
+							//	 commits += '</li>';
+							// }
 
-              break;
-            case 'CreateEvent':
-              action = 'created a new ' + item.payload.ref_type + ' in';
-              branch = ': <a target="_blank" href="https://github.com/' + item.repo.name + '/tree/' + item.payload.ref + '">' + item.payload.ref + '</a>';
-              break;
-            default:
-              console.log(item.type, item);
-              break;
-          }
+							// commits += '</ul>';
+							// console.log(item.type, item);
+							break;
+						case 'ForkEvent':
+							action = 'forked';
+							break;
+						case 'WatchEvent':
+							action = 'is now watching';
+							break;
+						case 'PullRequestEvent':
+							action	= item.payload.action + ' ';
+							action += 'pull request <a target="_blank" href="' + item.payload.pull_request.html_url + '">';
+							action += item.payload.pull_request.title;
+							action += '</a> for';
 
-          var entry = templater(template, {
-            gravatarSrc: gravatarSrc,
-            userUrl: makeHtmlUrl(item.actor.url),
-            username: username,
-            repoUrl: makeHtmlUrl(item.repo.url),
-            repoName: item.repo.name,
-            branch: branch,
-            created: item.created_at,
-            action: action,
-            commits: commits
-          });
+							break;
+						case 'CreateEvent':
+							action = 'created a new ' + item.payload.ref_type + ' in';
+							branch = ': <a target="_blank" href="https://github.com/' + item.repo.name + '/tree/' + item.payload.ref + '">' + item.payload.ref + '</a>';
+							break;
+						default:
+							console.log(item.type, item);
+							break;
+					}
 
-          $('<li/>').append(entry).appendTo($obj);
-        }
+					var entry = templater(template, {
+						gravatarSrc: gravatarSrc,
+						userUrl: makeHtmlUrl(item.actor.url),
+						username: username,
+						repoUrl: makeHtmlUrl(item.repo.url),
+						repoName: item.repo.name,
+						branch: branch,
+						created: item.created_at,
+						action: action,
+						commits: commits
+					});
 
-        if ('timeago' in $('abbr.timeago')) {
-          $('abbr.timeago').timeago();
-        }
-        if ('tooltip' in $('a[rel]')) {
-          $('a[rel]').tooltip();
-        }
-      });
-    });
-  };
+					$('<li/>').append(entry).appendTo($obj);
+				}
+
+				if ('timeago' in $('abbr.timeago')) {
+					$('abbr.timeago').timeago();
+				}
+				if ('tooltip' in $('a[rel]')) {
+					$('a[rel]').tooltip();
+				}
+			});
+		});
+	};
 }( jQuery ));
